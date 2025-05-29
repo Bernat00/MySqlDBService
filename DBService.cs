0350f61bc645835b79b4162838b69c17f4d28c50
Bernat00 <szabobernat06@gmail.com>
2025-05-29 23:27:41 +0200

    idk fel 12 van

--- DBService.cs
+++ DBService.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using MySql.Data.MySqlClient;
+﻿using MySqlConnector;
+using System;
 
 namespace MySqlDBService
 {
@@ -10,10 +10,19 @@ namespace MySqlDBService
         MySqlCommand cmd;
         MySqlDataReader reader;
 
+
+        public delegate void ActionThatWorksWithParams(params object[] args);
+
+
         public MySqlDataReader Reader
         {
             get
             {
+                if (cmd == null)
+                {
+                    throw new Exception("Nothing to read, cmd not yet created.");
+                }
+
                 if (reader == null || reader.IsClosed)
                 {
                     reader = cmd.ExecuteReader();
@@ -46,16 +55,19 @@ namespace MySqlDBService
             conn.Open();
         }
 
-        public static void SetDBConfig(string server, string userId, string db, string password = "", string charSet = "utf8")
+        public static void SetDBConfig(string server, string userId, string db, uint port = 3306, string password = "", string charSet = "utf8")
         {
             if (csb == null)
             {
-                csb = new MySqlConnectionStringBuilder();
-                csb.Server = server;
-                csb.UserID = userId;
-                csb.Password = password;
-                csb.Database = db;
-                csb.CharacterSet = charSet;
+                csb = new MySqlConnectionStringBuilder
+                {
+                    Server = server,
+                    Port = port,
+                    UserID = userId,
+                    Password = password,
+                    Database = db,
+                    CharacterSet = charSet
+                };
             }
         }
 
@@ -77,9 +89,39 @@ namespace MySqlDBService
             cmd.Parameters[name].Direction = System.Data.ParameterDirection.Input;
         }
 
+
+        public ActionThatWorksWithParams this[string procedureName]
+        {
+            get
+            {
+                void CreateProcedureCall(params object[] args)
+                {
+                    string vars = "";
+
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        string param = $"@{i}, ";
+                        vars += param;
+                    }
+
+                    vars = vars.TrimEnd(',', ' ');
+
+                    Query($"CALL {procedureName}({vars})");
+
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        AddInputParameter($"@{i}", args[i]);
+                    }
+                }
+
+                return CreateProcedureCall;
+            }
+        }
+
         ~DBService()
         {
             conn?.Close(); // ez meno (ha conn == null akk null-t ad vissza es nem kell if-ezni)
         }
+
     }
 }
